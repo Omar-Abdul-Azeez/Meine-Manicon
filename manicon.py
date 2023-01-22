1d5752483c6ffc33ed436db4c22419fa4c9a32ff
Omar Abdul Azeez <49833164+Omar-Abdul-Azeez@users.noreply.github.com>
2023-01-23 05:15:02 +0900

    Update manicon.py

--- manicon.py
+++ manicon.py
@@ -1,121 +1,14 @@
 import os
-import re
-from warnings import filterwarnings
+from time import sleep
 
+import cfscrape
+import regex
+import requests
 from PIL import Image, ImageOps
-from jikanpy import Jikan
-from requests import get
-
-print('''Run this in your anime folder
-For help, info and memes, check out
-https://github.com/Omar-Abdul-Azeez/Meine-Manicon
-''')
-
-jikan = Jikan()
-filterwarnings("ignore")
-folders = next(os.walk('.'))[1]
-type = os.getcwd().rpartition('\\')[2].lower()
-automode = True if input('Use AutoMode? Y/N : ').upper() == 'Y' else False
 
 
-def getname(name: str) -> str:
-    lastwords = ['BD', '480p', '720p', '1080p']
-    if "Season" in name:
-        name = name.partition(" - ")[2]
-    else:
-        for word in lastwords:
-            rexstr = word + "(?s)(.*$)"
-            name = re.sub(rexstr, '', name)
-
-    return name.strip()
-
-
-def getartwork(folder: str, name: str, type: str) -> tuple:
-    # print the current title
-    print('\n' + name.title(), end='')
-    # check if MAL ID is already found
-    if os.path.isfile(folder + "\\ID.txt"):
-        id = int(open(folder + "\\ID.txt", "r").read())
-    else:  # search 5 pages tops for the anime/manga
-        page = 1
-        results = jikan.search(type, name, page)
-
-        for page in range(1, min((results["last_page"], 5))):
-            if page != 1:
-                results = jikan.search(type, name, page)
-            counter = 1
-            for result in results['results']:
-                if automode:
-                    print(' - ' + result['title'])
-                    ch = "1"
-                    break
-                else:
-                    print('\n' + str(counter) + ' - ' + result['title'], end='')
-                    ch = "m"
-                    counter += 1
-                if counter % 5 == 0:
-                    print("\nm - more", end="")
-                    ch = input("\n>")
-                    counter += 1
-                    if ch != "m":
-                        break
-            else:
-                continue
-            break
-        else:
-            print("\nNo more", end="")
-            ch = input('\n>')
-        id = results['results'][int(ch) - 1]['mal_id']
-        open(folder + "\\ID.txt", "w").write(str(id))
-
-    folder = folder + "\\Artwork"
-
-    # pull pictures related to anime/manga
-    def getresults(ext: str = None):
-        if type == "anime":
-            return jikan.anime(id, extension=ext)
-        elif type == "manga":
-            return jikan.manga(id, extension=ext)
-
-    results = getresults("pictures")
-    # if autoMode just grab first picture and save in Artwork folder for later use (or use existing one)
-    if automode:
-        if not os.path.isfile(folder + "\\1.jpg"):
-            open(folder + "\\1.jpg", "wb").write(get(results["pictures"][0]["large"]).content)
-    else:  # grab existing pictures and list them for use
-        counter = 1
-        fileslist = next(os.walk(folder))[2]
-        if fileslist is not None:
-            for _ in range(len(fileslist)):
-                jpgfile = folder + "\\" + str(counter) + ".jpg"
-                print("\n - " + str(counter) + ".jpg", end="")
-                Image.open(jpgfile).show()
-                if counter % 5 == 0:
-                    print("\nm - more", end="")
-                    ch = input("\n>")
-                    if ch != "m":
-                        return folder + "\\" + ch + ".jpg", results["type"]
-                counter += 1
-        # if not yet chosen list the rest of the pictures online and save them for later use
-        for pic in results["pictures"][counter - 1:len(results["pictures"])]:
-            jpgfile = folder + "\\" + str(counter) + ".jpg"
-            open(jpgfile, "wb").write(get(pic["large"]).content)
-            print("\n - " + str(counter) + ".jpg", end="")
-            Image.open(jpgfile).show()
-            if counter % 5 == 0:
-                print("\nm - more", end="")
-                ch = input("\n>")
-                if ch != "m":
-                    break
-            counter += 1
-        else:
-            print("\nNo more", end="")
-            ch = input("\n>")
-    return folder + "\\" + ch + ".jpg", getresults()["type"]
-
-
-def createicon(jpgfile: str, iconfile: str):
-    img = Image.open(jpgfile)
+def createicon(folder: str, name: str, pic: str):
+    img = Image.open(pic)
     img = ImageOps.expand(img, (69, 0, 69, 0), fill=0)
     img = ImageOps.fit(img, (300, 300)).convert("RGBA")
 
@@ -128,48 +21,72 @@ def createicon(jpgfile: str, iconfile: str):
             newData.append(item)
 
     img.putdata(newData)
-    img.save(iconfile)
+    img.save(folder + "\\" + name + ".ico")
     img.close()
+    return folder + "\\" + name + ".ico"
 
 
-def process(folder: str, name: str, type: str):
-    iconfile = folder + '\\' + name.replace(' ', '_') + '.ico'
-
-    if os.path.isfile(iconfile):
-        print('An icon is already present. Delete the older icon before applying a new one')
+def process(folder: str):
+    name = regex.sub(r" \[anidb-\d+]", "", folder)
+    query = """
+    query ($name: String) {
+      Media (search: $name, type: ANIME) {
+        id
+        title {
+          romaji
+        }
+        bannerImage
+        coverImage {
+          extraLarge
+        }
+      }
+    }
+    """
+    variables = {
+        "name": name
+        }
+    url = 'https://graphql.anilist.co'
+    response = requests.post(url, json={'query': query, 'variables': variables}).json()["data"]["Media"]
+    id = response["id"]
+    banner = response["bannerImage"]
+    cover = response["coverImage"]["extraLarge"]
+    if not os.path.exists(folder + "\\Cover"):
+        os.mkdir(folder + "\\Cover")
+    scraper = cfscrape.create_scraper()
+    if not os.path.exists(folder + "\\Cover\\" + str(id) + "b.png"):
+        if banner:
+            bannerimg = scraper.get(banner)
+            with open(folder + "\\Cover\\" + str(id) + "b.png", 'wb') as f:
+                bannerimg.decode_content = True
+                f.write(bannerimg.content)
+    if not os.path.exists(folder + "\\Cover\\" + str(id) + "c.png"):
+        coverimg = scraper.get(cover)
+        with open(folder + "\\Cover\\" + str(id) + "c.png", 'wb') as f:
+            coverimg.decode_content = True
+            f.write(coverimg.content)
+    if os.path.exists(folder + "\\" + name + ".ico") or os.path.exists(folder + "\\desktop.ini"):
+        print(name + " Already done")
         return
-
-    jpgfile, type = getartwork(folder, name, type)
-
     try:
-        icon = createicon(jpgfile, iconfile)
+        icon = createicon(folder, name, folder + "\\Cover\\" + str(id) + "c.png")
     except:
-        print('Ran into an error. Blame the dev :(')
+        print("Ran into an error. Blame the Dev :(")
         return
 
-    f = open(folder + "\\desktop.ini", "w+")
-
+    f = open(folder + "\\desktop.ini", "w+", encoding="ANSI")
     f.write("[.ShellClassInfo]\nConfirmFileOp=0\n")
-    f.write("IconResource={},0".format(iconfile.replace(folder, "").strip("\\")))
-    f.write("\nIconFile={}\nIconIndex=0".format(iconfile.replace(folder, "").strip("\\")))
-
-    if type is not None and len(type) > 0:
-        # If the result has a type, then using this as the infotip for the desktop icon.
-        f.write("\nInfoTip={}".format(type))
-
-    # Closing the output stream. All the text will be written into `desktop.ini` file only when the output is being
-    # closed.
+    f.write(f"IconResource={name}.ico,0")
+    f.write(f"\nIconFile={name}.ico\nIconIndex=0")
     f.close()
-
-    # Not marking the `desktop.ini` file as a system file. This will make sure that the file can be seen if display
-    # hidden items is enabled.
-    os.system('attrib +r \"{}\\{}\"'.format(os.getcwd(), folder))
+    os.system('attrib +r \"{}\\{}\"'.format(os.getcwd(),folder))
     os.system('attrib +h \"{}\\desktop.ini\"'.format(folder))
     os.system('attrib +h \"{}\"'.format(icon))
 
 
+folders = next(os.walk('.'))[1]
+count = 0
 for folder in folders:
-    process(folder, getname(folder), type)
-    if type == "anime":
-        for minifolder in next(os.walk(folder + "\\Anime"))[1]:
-            process(folder + "\\Anime\\" + minifolder, getname(minifolder), "anime")
+    if (count == 89):
+        sleep(60)
+    process(folder)
+    count += 1
